c2b24044a219e53a4172ab4e4eebb99b788c72ec
RukshanNuwan <shanwijendra.dev@gmail.com>
2023-08-20 23:51:32 +0530

    created about component

--- components/jobdetails/about/About.jsx
+++ components/jobdetails/about/About.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text } from 'react-native';
 
-import styles from './about.style'
+import styles from './about.style';
 
-const About = () => {
+const About = ({ info }) => {
   return (
-    <View>
-      <Text>About</Text>
+    <View style={styles.container}>
+      <View style={styles.headText}>About the job:</View>
+      <View style={styles.contentBox}>
+        <Text style={styles.contextText}>{info}</Text>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default About
+export default About;
